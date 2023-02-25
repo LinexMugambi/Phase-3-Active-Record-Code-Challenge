416f54faff804957bf57bb371fb2bbfbc4e2f215
git_LinexMugambi <linexmugambi254@gmail.com>
2023-02-25 17:37:53 +0300

    updated seed.rb

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+puts "Deleting old data..."
+Product.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+user1 = User.create(name: Faker::Name.name)
+user2 = User.create(name: Faker::Name.name)
+user3 = User.create(name: Faker::Name.name)
+
+puts "Creating products..."
+product1 = Product.create(name: "Stapler", price: 10)
+product2 = Product.create(name: "Whiteboard", price: 15)
+product3 = Product.create(name: "Dry Erase Markers", price: 5)
+product4 = Product.create(name: "Ballpoint Pens", price: 2)
+product5 = Product.create(name: "Scotch Tape", price: 3)
+
+puts "Creating reviews..."
+
+
+puts "Seeding done!"
